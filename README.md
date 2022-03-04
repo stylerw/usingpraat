ae6a8c970c684e06fede22011c2e5ef0a06b8e3f
Will Styler <will@savethevowels.org>
2022-03-04 10:13:15 -0800

    Initial Open Release
    This is the initial release allowing public contribution to the document.  More to come, but feel free to submit pull requests in the mean time :)

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# usingpraat
-The public respository for the open educational resource 'Using Praat for Linguistic Research'
+# Using Praat for Linguistic Research
+
+This is the official Github page for the Open Educational Resource 'Using Praat for Linguistic Research', written by Will Styler.
+
+## About the document
+
+For more details on the resource, as well as some accompanying materials, go to <https://savethevowels.org/praat>.
+
+## The format of the document source
+
+This is a hybrid XeLaTeX/Markdown document, with the main document being `usingpraat.md`.  The bulk of the text is written in [Markdown](https://en.wikipedia.org/wiki/Markdown), but complex formatting, tables, and references are rendered in LaTeX.  This is possible using [pandoc](https://pandoc.org/). In general, your contribution text should use Markdown, and rely on TeX only for citations, figures, and references.
+
+All figures are in the 'build' folder, and all bibliographic information is in 'usingpraat.bib'.
+
+## Rendering the Document
+
+To render this document from the source, clone this repository, make sure you have (minimally) XeLaTeX, Bibtex, pandoc and perl around, and then run `bash render_using_praat.sh` on your local machine.  This assumes you have a working Unix environment, which you really should :)
+
+After some chugging, it will render the latest document.
+
+## How to contribute
+
+If you find a typo, formatting issue, or other problem in the document, please feel free to submit a pull request which makes the change, following the formatting guidelines above.
+
+If you'd like to make a more substantial contribution, please feel free to revise both the `usingpraat.md`, `usingpraat.bib`, and to add any additional figures to `build`. Any text and figures should feature only your own work (or properly cited quotes of others' work), and by contributing, you assert that your contributions will fall under the document's Creative Commons license.
+
+I'm still revising the document's text to reflect a multi-author collaborative environment, but you will be duly credited for your work.
+
+Finally, although pull requests are welcome, I reserve the right to modify, revise, or reject any requests for any reasons I see fit.
+
+
---
+++ render_using_praat.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+# This script builds Using Praat for Linguistic research on any Unix-based machine with both Pandoc and XeLaTeX and Perl installed.
+
+# Captures the date for later use
+date=`date +"%m-%d-%y-%H%M"`
+
+# Move into the build folder
+cd build
+# Make a copy of the markdown file for processing (safety measure in case of scripting errors)
+cp ../usingpraat.md usingpraatbuild.md
+cp ../usingpraat.bib bibtex.bib
+
+# Run Pandoc to turn the markdown file with the bulk of the document into a .TeX file
+pandoc -f markdown+raw_tex --pdf-engine=xelatex  -i usingpraatbuild.md -o pandocked.tex
+
+# Remove some of the junk that Markdown adds when converting to TeX.
+sed -i .bak 's/\\tightlist//g' pandocked.tex
+sed -i .bak 's/\[<+->\]//g' pandocked.tex
+sed -i .bak 's/\\def\\labelenumi{\\arabic{enumi}.}//g' pandocked.tex
+sed -i .bak 's/\\itemsep1pt\\parskip0pt\\parsep0pt//g' pandocked.tex
+sed -i .bak 's/-\\textgreater{}/\$\\to\$/g' pandocked.tex
+
+# Prettify F0 writing, and also make fake arrows into real arrows
+
+perl -pi -w -e 's/F0/F\$_{0}\$/g;' pandocked.tex
+perl -pi -w -e 's/-\textgreater{}/\$\to\$/g;' pandocked.tex
+
+# Concatenate the header file (with the preambles, TOC, etc), the pandoc-created TeX file, and the footer file (with the bibliography) into a single buildable TeX file
+cat header.tex pandocked.tex footer.tex > usingpraat.tex
+
+# Build the TeX once without stopping for errors (as the hyperref plugin throws errors on the first run)
+/Library/TeX/texbin/xelatex -output-driver="/Library/TeX/texbin/xdvipdfmx" -interaction=nonstopmode -synctex=1 usingpraat
+
+# Render the bibliography based on the prior file
+/Library/TeX/texbin/bibtex usingpraat
+
+# Render the file twice more, to ensure that the bibliographical references are included and that the TOC reflects everything accurately
+/Library/TeX/texbin/xelatex -output-driver="/Library/TeX/texbin/xdvipdfmx" -synctex=1 usingpraat
+/Library/TeX/texbin/xelatex -output-driver="/Library/TeX/texbin/xdvipdfmx" -synctex=1 usingpraat
+
+# Open the PDF generated 
+open usingpraat.pdf
+
+# Delete the TeX files
+rm bibtex.bib
+rm usingpraatbuild.md
+rm usingpraat.aux
+rm usingpraat.bbl
+rm usingpraat.blg
+rm usingpraat.log
+rm usingpraat.out
+rm usingpraat.synctex.gz
+rm usingpraat.thm
+rm usingpraat.toc
+rm usingpraat.tex
+rm pandocked.tex
+rm pandocked.tex.bak
+
+# Move the PDF and final TeX out of the build folder for accessibility
+mv usingpraat.pdf ../UsingPraatforLinguisticResearchLatest.pdf
